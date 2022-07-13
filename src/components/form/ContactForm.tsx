ac913a0c9b9c2d30b789d0883e072178e7918cea
anthonidev <softwaretoni21@gmail.com>
2022-07-12 19:57:26 -0500

    colors

--- src/components/form/ContactForm.tsx
+++ src/components/form/ContactForm.tsx
@@ -2,107 +2,121 @@ import React from 'react'
 
 const ContactForm = () => {
     return (
-        <section className="bg-gray-100">
-            <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
-                <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
-                    <div className="lg:py-12 lg:col-span-2">
-                        <p className="max-w-xl text-lg">
-                            At the same time, the fact that we are wholly owned and totally independent from manufacturer and other group
-                            control gives you confidence that we will only recommend what is right for you.
-                        </p>
-
-                        <div className="mt-8">
-                            <a href="" className="text-2xl font-bold text-pink-600"> 0151 475 4450 </a>
-
-                            <address className="mt-2 not-italic">282 Kevin Brook, Imogeneborough, CA 58517</address>
-                        </div>
-                    </div>
-
-                    <div className="p-8 bg-white rounded-lg shadow-lg lg:p-12 lg:col-span-3">
-                        <form action="" className="space-y-4">
-                            <div>
-                                <label className="sr-only" htmlFor="name">Name</label>
-                                <input className="w-full p-3 text-sm border-gray-200 rounded-lg" placeholder="Name" type="text" id="name" />
-                            </div>
-
-                            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                                <div>
-                                    <label className="sr-only" htmlFor="email">Email</label>
-                                    <input
-                                        className="w-full p-3 text-sm border-gray-200 rounded-lg"
-                                        placeholder="Email address"
-                                        type="email"
-                                        id="email"
-                                    />
-                                </div>
-
-                                <div>
-                                    <label className="sr-only" htmlFor="phone">Phone</label>
-                                    <input
-                                        className="w-full p-3 text-sm border-gray-200 rounded-lg"
-                                        placeholder="Phone Number"
-                                        type="tel"
-                                        id="phone"
-                                    />
-                                </div>
-                            </div>
-
-                            <div className="grid grid-cols-1 gap-4 text-center sm:grid-cols-3">
-                                <div>
-                                    <input className="sr-only" id="option1" type="radio" tabIndex={-1} />
-                                    <label htmlFor="option1" className="block w-full p-3 border border-gray-200 rounded-lg" tabIndex={0}>
-                                        <span className="text-sm font-medium"> Option 1 </span>
-                                    </label>
-                                </div>
-
-                                <div>
-                                    <input className="sr-only" id="option2" type="radio" tabIndex={-1} />
-                                    <label htmlFor="option2" className="block w-full p-3 border border-gray-200 rounded-lg" tabIndex={0}>
-                                        <span className="text-sm font-medium"> Option 2 </span>
-                                    </label>
-                                </div>
-
-                                <div>
-                                    <input className="sr-only" id="option3" type="radio" tabIndex={-1} />
-                                    <label htmlFor="option3" className="block w-full p-3 border border-gray-200 rounded-lg" tabIndex={0}>
-                                        <span className="text-sm font-medium"> Option 3 </span>
-                                    </label>
-                                </div>
-                            </div>
-
-                            <div>
-                                <label className="sr-only" htmlFor="message">Message</label>
-                                <textarea
-                                    className="w-full p-3 text-sm border-gray-200 rounded-lg"
-                                    placeholder="Message"
-                                    rows={8}
-                                    id="message"
-                                ></textarea>
-                            </div>
-
-                            <div className="mt-4">
-                                <button
-                                    type="submit"
-                                    className="inline-flex items-center justify-center w-full px-5 py-3 text-white bg-black rounded-lg sm:w-auto"
-                                >
-                                    <span className="font-medium"> Send Enquiry </span>
-
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="w-5 h-5 ml-3"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                    >
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+        <div className="relative">
+      <img
+        src="https://images.pexels.com/photos/3228766/pexels-photo-3228766.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+        className="absolute inset-0 object-cover w-full h-full"
+        alt=""
+      />
+      <div className="relative bg-opacity-75 bg-deep-purple-accent-700">
+        <svg
+          className="absolute inset-x-0 bottom-0 text-white"
+          viewBox="0 0 1160 163"
+        >
+          <path
+            fill="currentColor"
+            d="M-164 13L-104 39.7C-44 66 76 120 196 141C316 162 436 152 556 119.7C676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C76 162.5 -44 162.5 -104 162.5H-164V13Z"
+          />
+        </svg>
+        <div className="relative px-4 py-16 mx-auto overflow-hidden sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+          <div className="flex flex-col items-center justify-between xl:flex-row">
+            <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
+              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+                The quick, brown fox <br className="hidden md:block" />
+                jumps over a lazy dog
+              </h2>
+              <p className="max-w-xl mb-4 text-base text-gray-200 md:text-lg">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudan, totam rem aperiam, eaque ipsa
+                quae.
+              </p>
+              <a
+                href="/"
+                aria-label=""
+                className="inline-flex items-center font-semibold tracking-wider transition-colors duration-200 text-teal-accent-400 hover:text-teal-accent-700"
+              >
+                Learn more
+                <svg
+                  className="inline-block w-3 ml-2"
+                  fill="currentColor"
+                  viewBox="0 0 12 12"
+                >
+                  <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+                </svg>
+              </a>
             </div>
-        </section>
+            <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
+              <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
+                <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
+                  Sign up for updates
+                </h3>
+                <form>
+                  <div className="mb-1 sm:mb-2">
+                    <label
+                      htmlFor="firstName"
+                      className="inline-block mb-1 font-medium"
+                    >
+                      First name
+                    </label>
+                    <input
+                      placeholder="John"
+                      required
+                      type="text"
+                      className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      id="firstName"
+                      name="firstName"
+                    />
+                  </div>
+                  <div className="mb-1 sm:mb-2">
+                    <label
+                      htmlFor="lastName"
+                      className="inline-block mb-1 font-medium"
+                    >
+                      Last name
+                    </label>
+                    <input
+                      placeholder="Doe"
+                      required
+                      type="text"
+                      className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      id="lastName"
+                      name="lastName"
+                    />
+                  </div>
+                  <div className="mb-1 sm:mb-2">
+                    <label
+                      htmlFor="email"
+                      className="inline-block mb-1 font-medium"
+                    >
+                      E-mail
+                    </label>
+                    <input
+                      placeholder="john.doe@example.org"
+                      required
+                      type="text"
+                      className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                      id="email"
+                      name="email"
+                    />
+                  </div>
+                  <div className="mt-4 mb-2 sm:mb-4">
+                    <button
+                      type="submit"
+                      className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                    >
+                      Subscribe
+                    </button>
+                  </div>
+                  <p className="text-xs text-gray-600 sm:text-sm">
+                    We respect your privacy. Unsubscribe at any time.
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,10 +39,215 @@ const Home: NextPage = () => {
           </div>
         </div>
 
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+          <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+            <div>
+              <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+                Servicios
+              </p>
+            </div>
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+              <span className="relative inline-block">
+                <svg
+                  viewBox="0 0 52 24"
+                  fill="currentColor"
+                  className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                >
+                  <defs>
+                    <pattern
+                      id="34f481be-159a-4846-821d-9ca19fb6bcc5"
+                      x="0"
+                      y="0"
+                      width=".135"
+                      height=".30"
+                    >
+                      <circle cx="1" cy="1" r=".7" />
+                    </pattern>
+                  </defs>
+                  <rect
+                    fill="url(#34f481be-159a-4846-821d-9ca19fb6bcc5)"
+                    width="52"
+                    height="24"
+                  />
+                </svg>
+                <span className="relative">Diseñamos</span>
+              </span>{' '}
+              páginas web optimizadas para Google
+            </h2>
+            <p className="text-base text-gray-700 md:text-lg">
+              Somos peruanos especialistas en diseño y posicionamiento Web
+            </p>
+          </div>
+          <div className="grid gap-8 row-gap-5 mb-8 md:row-gap-8 lg:grid-cols-4 sm:grid-cols-2">
+            <div className="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 hover:-translate-y-2">
+              <div className="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                <h6 className="mb-2 font-semibold leading-5">Diseño de páginas web</h6>
+                <p className="text-sm text-gray-900">
+                  Sportacus andrew weatherall goose Refined gentlemen super mario
+                  des lynam alpha trion zap rowsdower.
+                </p>
+              </div>
+            </div>
+            <div className="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 hover:-translate-y-2">
+              <div className="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                <h6 className="mb-2 font-semibold leading-5">Desarrollo de tiendas online</h6>
+                <p className="text-sm text-gray-900">
+                  Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+              </div>
+            </div>
+            <div className="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 hover:-translate-y-2">
+              <div className="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                <h6 className="mb-2 font-semibold leading-5">
+                  Diseño de Landing Pages
+                </h6>
+                <p className="text-sm text-gray-900">
+                  Bro ipsum dolor sit amet gaper backside single track, manny Bike
+                  epic clipless. Schraeder drop gondy.
+                </p>
+              </div>
+            </div>
+            <div className="duration-300 transform bg-white border-l-4 border-deep-purple-accent-400 hover:-translate-y-2">
+              <div className="h-full p-5 border border-l-0 rounded-r shadow-sm">
+                <h6 className="mb-2 font-semibold leading-5">Páginas web económicas</h6>
+                <p className="text-sm text-gray-900">
+                  A flower in my garden, a mystery in my panties. Heart attack never
+                  stopped old Big Bear.
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+
+
         {
           <ContactForm />
         }
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+          <div className="grid gap-5 row-gap-8 lg:grid-cols-2">
+            <div className="flex flex-col justify-center">
+              <div className="max-w-xl mb-6">
+                <div>
+                  <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+                    Brand new
+                  </p>
+                </div>
+                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                  Innovative analytics
+                  <br className="hidden md:block" />
+                  that you{' '}
+                  <span className="inline-block text-deep-purple-accent-400">
+                    will love
+                  </span>
+                </h2>
+                <p className="text-base text-gray-700 md:text-lg">
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                  quae. explicabo.
+                </p>
+              </div>
+              <a
+                href="/"
+                aria-label=""
+                className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+              >
+                Learn more
+                <svg
+                  className="inline-block w-3 ml-2"
+                  fill="currentColor"
+                  viewBox="0 0 12 12"
+                >
+                  <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+                </svg>
+              </a>
+            </div>
+            <div className="relative">
+              <svg
+                className="absolute w-full text-teal-accent-400"
+                fill="currentColor"
+                viewBox="0 0 600 392"
+              >
+                <rect x="0" y="211" width="75" height="181" rx="8" />
+                <rect x="525" y="260" width="75" height="132" rx="8" />
+                <rect x="105" y="83" width="75" height="309" rx="8" />
+                <rect x="210" y="155" width="75" height="237" rx="8" />
+                <rect x="420" y="129" width="75" height="263" rx="8" />
+                <rect x="315" y="0" width="75" height="392" rx="8" />
+              </svg>
+              <svg
+                className="relative w-full text-deep-purple-accent-400"
+                fill="currentColor"
+                viewBox="0 0 600 392"
+              >
+                <rect x="0" y="311" width="75" height="81" rx="8" />
+                <rect x="525" y="351" width="75" height="41" rx="8" />
+                <rect x="105" y="176" width="75" height="216" rx="8" />
+                <rect x="210" y="237" width="75" height="155" rx="8" />
+                <rect x="420" y="205" width="75" height="187" rx="8" />
+                <rect x="315" y="83" width="75" height="309" rx="8" />
+              </svg>
+            </div>
+          </div>
+        </div>
 
+
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-5 row-gap-8 lg:grid-cols-2">
+        <div className="flex flex-col justify-center">
+          <div className="max-w-xl mb-6">
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              The quick, brown fox
+              <br className="hidden md:block" />
+              jumps over{' '}
+              <span className="relative px-1">
+                <div className="absolute inset-x-0 bottom-0 h-3 transform -skew-x-12 bg-teal-accent-400" />
+                <span className="relative inline-block text-deep-purple-accent-400">
+                  a lazy dog
+                </span>
+              </span>
+            </h2>
+            <p className="text-base text-gray-700 md:text-lg">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae. explicabo.
+            </p>
+          </div>
+          <div className="grid gap-5 row-gap-8 sm:grid-cols-2">
+            <div className="bg-white border-l-4 shadow-sm border-deep-purple-accent-400">
+              <div className="h-full p-5 border border-l-0 rounded-r">
+                <h6 className="mb-2 font-semibold leading-5">
+                  I'll be sure to note that in my log
+                </h6>
+                <p className="text-sm text-gray-900">
+                  Lookout flogging bilge rat main sheet bilge water nipper fluke
+                  to go on account heave down.
+                </p>
+              </div>
+            </div>
+            <div className="bg-white border-l-4 shadow-sm border-deep-purple-accent-400">
+              <div className="h-full p-5 border border-l-0 rounded-r">
+                <h6 className="mb-2 font-semibold leading-5">
+                  A business big enough that it could be listed
+                </h6>
+                <p className="text-sm text-gray-900">
+                  Those options are already baked in with this model shoot me an
+                  email clear.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img
+            className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
+            src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
         <header className=" max-w-7xl  mx-auto ">
           <div className='flex  md:flex-row flex-col justify-center items-center h-full'>
             <div className='px-4 py-2  max-w-lg flex flex-col '>
